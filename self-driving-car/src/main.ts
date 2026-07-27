9ab96537728e68e7d25c58e274733a42267ca885
HeberDsouza <lourenco.h.colares@gmail.com>
2026-07-26 23:04:17 -0300

    course: 1.2 - Defining the Car class

--- self-driving-car/src/main.ts
+++ self-driving-car/src/main.ts
@@ -1,5 +1,9 @@
+import { Car } from "./classes/Car";
+
 const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 canvas.height = window.innerHeight;
 canvas.width = 200;
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
