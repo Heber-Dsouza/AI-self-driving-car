51afbe45630e61caa4bbf6798322929cf23b8ae5
HeberDsouza <lourenco.h.colares@gmail.com>
2026-07-26 23:43:31 -0300

    course: 1.3 - Implementing the Controls class

--- self-driving-car/src/classes/Car.ts
+++ self-driving-car/src/classes/Car.ts
@@ -1,14 +1,24 @@
+import { Controls } from "./Controls";
+
 export class Car {
   #x: number;
   #y: number;
   width: number;
   height: number;
+  #controls: Controls;
 
   constructor(x: number, y: number, width: number, height: number) {
     this.#x = x;
     this.#y = y;
     this.width = width;
     this.height = height;
+
+    this.#controls = new Controls();
+  }
+
+  update() {
+    if (this.#controls.forward) this.#y -= 1;
+    if (this.#controls.reverse) this.#y += 1;
   }
 
   draw(ctx: CanvasRenderingContext2D) {
